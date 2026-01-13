5c40f115a3c4d19b32a4f36f261cf4aa2bd95c86
Leo Nguyen <leo.t.beast@gmail.com>
2026-01-12 22:39:32 -0800

    YES JOYCON BUTTON WORKS LFG js have to fix axis -- not even that bad?

--- remotewebsite.js
+++ remotewebsite.js
@@ -19,6 +19,33 @@ function send() {
 //controllerside
 
 
-gameControl.on("connect",gamepad => {
-	console.log("gamepad connection!")
-})
+window.addEventListener("gamepadconnected", (event) => {
+	console.log("gamepad connected");
+	console.log(event.gamepad.id)
+});
+
+//loop to check state
+
+function pollGamepad() {
+	
+	const gamepads = navigator.getGamepads();
+	
+	//loop to check nulls and not proceed
+	
+	for (let gp of gamepads) {
+		if (gp === null) continue;
+	
+	
+		if (gp.id.includes("Joy-Con")) {
+			const xAxis = gp.axes[0];
+			const stopBut = gp.buttons[0].pressed;
+			//js rounds for printing, dont use for math -- add a system which takes certain thresholds and determines direction
+			console.log(xAxis.toFixed,stopBut);
+		}
+		//recursive-type infinite call
+	}
+	requestAnimationFrame(pollGamepad);
+	
+}
+
+pollGamepad();
